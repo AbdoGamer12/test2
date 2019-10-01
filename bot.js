3bd147021d2775af1c969b4b99a6487df3e13d75
AbdoGamer12 <56035535+AbdoGamer12@users.noreply.github.com>
2019-10-01 16:56:12 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("628594026311843882
+")
+setInterval(function() {
+channel.send(`Abdo_Gamer Abdo_Gamer Abdo_Gamer Abdo_Gamer`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
